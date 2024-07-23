7b74ed6fef00497459e195c0b0c02be0a407e1de
Hsu <seayxu@gmail.com>
2024-07-23 15:59:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.choas.eu.org": dockerHub,
+  "quay.choas.eu.org": "https://quay.io",
+  "gcr.choas.eu.org": "https://gcr.io",
+  "k8s-gcr.choas.eu.org": "https://k8s.gcr.io",
+  "k8s.choas.eu.org": "https://registry.k8s.io",
+  "ghcr.choas.eu.org": "https://ghcr.io",
+  "cloudsmith.choas.eu.org": "https://docker.cloudsmith.io",
+  "ecr.choas.eu.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.choas.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
